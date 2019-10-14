ec15759db11fa0ad022db52c4df4c3c0ac1084b5
Viktor Nareiko <viktor.nareiko@adaptagency.com>
2019-10-14 16:41:09 +0300

    Added examples how to use some of mocked endpoints

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { take } from 'lodash';
+
+import ApiFactory from './mock';
+import Editor from './Editor';
 
 export default function App() {
+  const [value, setValue ] = useState("");
+  console.log(value);
   return (
-    <div>
-      Hello from task :)
+    <div className="flexContainer">
+      <div className="flexChild rowParent">
+        <div className="flexChild columnParent" >
+          <div className="buttonsPanel">
+            <button onClick={ getIsAdmin }>Test `GET` /api/is-admin</button>
+            <button onClick={ getAllProducts }>Test `GET` /api/products</button>
+          </div>
+          <div className="flexChild rowParent">
+            <Editor value={value} />
+          </div>
+        </div>
+      </div>
     </div>
   );
+
+  function getIsAdmin() {
+    ApiFactory.getInstance().get('/api/is-admin')
+      .then(({ data }) => {
+        setValue(beautify(data));
+      })
+      .catch((error) => {
+        // TODO: Display nice error message.
+        console.log(error.response);
+      });
+  }
+
+  function getAllProducts() {
+    ApiFactory.getInstance().get('/api/products')
+      .then(({ data }) => {
+        setValue(beautify({ products: take(data, 3) }));
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
+
+  function beautify(val) {
+    return JSON.stringify(val, null, 2);
+  }
 }
